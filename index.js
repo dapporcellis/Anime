20732c438573df6eb274093d1d5878870d0fb7b5
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2021-07-20 10:06:03 -0300

    Coloquei novavamente o nome correto da rota usuario

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-var usuario = require("./routes/usuarioRoute");
+var usuarioRoute = require("./routes/usuarioRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 
-app.use("/usuario", usuario);
+app.use("/usuario", usuarioRoute);
 
 app.listen("3000", function () {
   console.log("Projeto funcionando!");
